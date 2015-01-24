28e9d3b7cdb0ffafa26d5e2a3166c7350f5e3a0e
toddnestor <toddnestor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-24 04:51:33 +0000

    version updated on plugin file
    git-svn-id: https://plugins.svn.wordpress.org/hercules-sentiment-analysis/tags/1.0.0@1074662 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hercules_sentiment_analysis.php
+++ hercules_sentiment_analysis.php
@@ -4,7 +4,7 @@ Plugin Name: Hercules Sentiment Analysis
 Description: Adds a sentiment analysis tools that show the overal sentiment (positive, negative, neutral) of comments, posts, and titles so you can better target your audience and respond to their needs.
 Author: Todd D. Nestor - todd.nestor@gmail.com
 Author URI: http://toddnestor.com
-Version: 1.1
+Version: 1.0.0
 License: GNU General Public License v3 or later
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 */
